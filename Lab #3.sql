792dfb9efef4ba152d437fd48ea0c26a2d81d43f
ncomito3 <nicholas.comito1@marist.edu>
2016-09-22 09:57:37 -0400

    Nicholas Comito: Lab #3 Starting SQL - 9/22/16
    Lab #3 
    Getting Started with SQL queries

---
+++ Lab #3.sql
@@ -0,0 +1,57 @@
+﻿--Nicholas Comito Lab #3; Due 9/22/16--
+
+--List the order number and total dollars of all orders.--
+select ordnum, totalUSD
+from orders;
+
+--List the name and city of agents named Smith.--
+select name, city
+from agents
+where name = 'Smith';
+
+--List the id, name, and priceUSD of products with quantity more than 201,000.--
+select pid, name, priceUSD
+from products
+where quantity > 201000;
+
+--List the names and cities of customers in Duluth.--
+select name, city
+from customers
+where city = 'Duluth';
+
+--List the names of agents not in New York and not in Duluth.--
+select name
+from agents
+where city NOT IN ('New York','Duluth');
+
+--List all data for products in neither Dallas nor Duluth that cost US$1 or more.--
+select *
+from products
+where 
+	city NOT IN ('Dallas','Duluth')
+		and 
+	priceUSD >= 1.00;
+
+
+--List all data for orders in February or March.--
+select *
+from orders
+where 
+	( mon = 'feb'
+	     or
+	  mon = 'mar');
+
+--List all data for orders in February of US$600 or more.--
+select *
+from orders 
+where 
+	( mon = 'feb')
+	     and 
+	( totalUSD >= 600);
+
+--List all orders from the customer whose cid is C005.--
+select *
+from orders
+where cid = 'c005';
+
+	
